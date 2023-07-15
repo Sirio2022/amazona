5782ec0cbfeb5f24e8eb831197a1f2a88149650e
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2023-07-15 12:41:01 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,11 @@ import data from './data.js';
 const app = express();
 
 // Configuring CORS
-const whitelist = ['http://localhost:5173', 'http://localhost:3000'];
+const whitelist = ['http://localhost:5173', 'http://localhost:5000'];
 
 const corsOptions = {
   origin: function (origin, callback) {
-    if(whitelist.includes(origin)){
+    if (whitelist.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
