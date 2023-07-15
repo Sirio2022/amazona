8791590ae6f8f9d9b277883f821748ead0271d84
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2023-07-15 12:33:35 -0500

    Brutal

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,23 @@
 import express from 'express';
+import cors from 'cors';
 import data from './data.js';
 
 const app = express();
+
+// Configuring CORS
+const whitelist = ['http://localhost:5173', 'http://localhost:3000'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if(whitelist.includes(origin)){
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 app.get('/api/products', (req, res) => {
   res.send(data.products);
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,12 +1,25 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import data from '../data';
+import axios from 'axios';
 
 export default function HomeScreen() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios(
+        import.meta.env.VITE_BACKEND_URL + '/api/products'
+      );
+      setProducts(data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
       <h1>Featured Products</h1>
       <div className="products">
-        {data.products.map((product) => (
+        {products.map((product) => (
           <div className="product" key={product.id}>
             <Link to={`/product/${product.slug}`}>
               <img src={product.image} alt={product.name} />
