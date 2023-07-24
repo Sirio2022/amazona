7c1f9f648c445f80454eb4327534d9e0f77179a3
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2023-07-24 15:29:48 -0500

    Update Product.jsx

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Rating from './Rating';
 
 export default function Product(children) {
@@ -5,13 +6,13 @@ export default function Product(children) {
 
   return (
     <div className="card" key={product._id}>
-      <a href={`/product/${product._id}`}>
+      <Link to={`/product/${product._id}`}>
         <img className="medium" src={product.image} alt={product.name} />
-      </a>
+      </Link>
       <div className="card-body">
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <h2>{product.name}</h2>
-        </a>
+        </Link>
 
         <Rating rating={product.rating} numReviews={product.numReviews} />
 
