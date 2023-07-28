f59284a4a2e1027d60fb51da3a288227fa563bc1
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2023-07-27 21:23:41 -0500

    Ahii vamos!

--- frontend/src/components/MessageBox.jsx
+++ frontend/src/components/MessageBox.jsx
@@ -1,7 +1,14 @@
-export default function MessageBox(children) {
+import PropTypes from 'prop-types';
+
+export default function MessageBox({ alert }) {
+  console.log('alert', alert);
   return (
-    <div className={`alert alert-${children.variant || 'info'}`}>
-      {children.children}
+    <div className={alert.error ? 'alert alert-danger' : 'alert alert-info'}>
+      {alert.msg}
     </div>
   );
 }
+
+MessageBox.propTypes = {
+  alert: PropTypes.object.isRequired,
+};
--- frontend/src/screens/RegisterScreen.jsx
+++ frontend/src/screens/RegisterScreen.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { register } from '../redux/registerSlice';
@@ -12,103 +12,119 @@ export default function RegisterScreen() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confimPassword, setConfimPassword] = useState('');
-  const [errorMSG, setErrorMSG] = useState('');
-  const [infoMSG, setInfoMSG] = useState('');
+  const [alert, setAlert] = useState({});
 
   const { userInfo, loading, error } = useSelector((state) => state.register);
+  console.log(error);
 
   const dispatch = useDispatch();
 
   const submitHandler = (e) => {
     e.preventDefault();
-    if (password !== confimPassword) {
-      alert('Password and Confirm Password Are Not Matched');
-    } else {
-      dispatch(register(name, email, password));
+
+    if ([name, email, password, confimPassword].includes('')) {
+      setAlert({
+        msg: 'All fields are required',
+        error: true,
+      });
+      return;
     }
+
+    if (password !== confimPassword) {
+      setAlert({
+        msg: 'Passwords do not match',
+        error: true,
+      });
+      return;
+    }
+    if (password.length < 6) {
+      setAlert({
+        msg: 'Password must be at least 6 characters',
+        error: true,
+      });
+      return;
+    }
+    setAlert({});
+
+    dispatch(register(name, email, password));
+    setAlert({
+      msg: error,
+      error: true,
+    });
   };
 
-  useEffect(() => {
-    if (userInfo.msg) {
-      setInfoMSG(userInfo.msg);
-    }
-  }, [userInfo, loading]);
-
-  useEffect(() => {
-    if (error) {
-      setErrorMSG(error);
-      setTimeout(() => {
-        setErrorMSG('');
-      }, 5000);
-    }
-  }, [loading, error]);
+  const { msg } = alert;
 
   return (
-    <div>
-      <form className="form" onSubmit={submitHandler}>
-        <div>
-          <h1>Register</h1>
-        </div>
-        {loading && <LoadingBox></LoadingBox>}
-        {infoMSG && <MessageBox variant="info">{infoMSG}</MessageBox>}
-        {errorMSG && <MessageBox variant="danger">{errorMSG}</MessageBox>}
-        <div>
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            id="name"
-            placeholder="Enter your name"
-            required
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="email">Email address</label>
-          <input
-            type="email"
-            id="email"
-            placeholder="Enter your Email"
-            required
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            placeholder="Enter your password"
-            required
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="confirm-password">Confirm Password</label>
-          <input
-            type="password"
-            id="confim-password"
-            placeholder="Repeat your password"
-            required
-            onChange={(e) => setConfimPassword(e.target.value)}
-          />
-        </div>
-        <div>
-          <label />
-          <button className="primary" type="submit">
-            Register
-          </button>
-        </div>
-        <div className="row">
-          <label />
+    <>
+      <div>
+        <form className="form" onSubmit={submitHandler}>
           <div>
-            Already have an account?{' '}
-            <Link to="/signin">Create new account</Link>
+            <h1>Register</h1>
+          </div>
+          {msg && <MessageBox alert={alert} />}
+          <div>
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={name}
+              placeholder="Enter your name"
+              onChange={(e) => setName(e.target.value)}
+            />
           </div>
           <div>
-            <Link to="/forgot-password">Sign In</Link>
+            <label htmlFor="email">Email address</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Enter your Email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
-        </div>
-      </form>
-    </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Enter your password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="confirm-password">Confirm Password</label>
+            <input
+              type="password"
+              id="confirm-password"
+              name="confim-password"
+              value={confimPassword}
+              placeholder="Repeat your password"
+              onChange={(e) => setConfimPassword(e.target.value)}
+            />
+          </div>
+          <div>
+            <label />
+            <button className="primary" type="submit">
+              Register
+            </button>
+          </div>
+          <div className="row">
+            <label />
+            <div>
+              Already have an account?{' '}
+              <Link to="/signin">Create new account</Link>
+            </div>
+            <div>
+              <Link to="/forgot-password">Sign In</Link>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
   );
 }
