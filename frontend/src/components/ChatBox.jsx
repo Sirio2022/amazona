dcec25eb9895531dd92cdf907bf6d9f09dc2387a
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2023-08-30 16:26:07 -0500

    Update ChatBox.jsx

--- frontend/src/components/ChatBox.jsx
+++ frontend/src/components/ChatBox.jsx
@@ -3,8 +3,9 @@ import socketIOClient from 'socket.io-client';
 
 const ENDPOINT =
   window.location.host.indexOf('localhost') >= 0
-    ? 'http://127.0.0.1:8000'
+    ? 'http://localhost:8000'
     : window.location.host;
+    
 
 export default function ChatBox(children) {
   const { userInfo } = children;
